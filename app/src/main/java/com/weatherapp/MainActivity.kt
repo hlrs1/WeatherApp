9ffbe1f27205efd734347ad9d92a216e8f25c30b
Herbert Santos <hlrs1@discente.ifpe.edu.br>
2025-01-02 10:10:05 -0300

    Prática 02 - Parte 1 - Criando Novas telas

--- app/src/main/java/com/weatherapp/MainActivity.kt
+++ app/src/main/java/com/weatherapp/MainActivity.kt
@@ -1,26 +1,14 @@
 package com.weatherapp
 
-import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import com.weatherapp.ui.HomePage
 import com.weatherapp.ui.theme.WeatherAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,31 +22,6 @@ class MainActivity : ComponentActivity() {
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
-            }
-        }
-    }
-}
-
-@Composable
-fun HomePage( modifier: Modifier = Modifier) {
-    val activity = LocalContext.current as? Activity
-    Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = CenterHorizontally,
-        modifier = modifier.padding(16.dp).fillMaxSize(),
-    ) {
-        Text(
-            text = "Bem Vindo!",
-            modifier = modifier
-        )
-        Spacer(modifier = modifier.size(24.dp))
-        Row(modifier = modifier) {
-            Button(
-                onClick = {
-                    activity?.finish()
-                }
-            ) {
-                Text("Sair")
             }
         }
     }
