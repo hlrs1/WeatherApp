6fb92b2e8729577e99971c343011e1ff90671bc3
Herbert Santos <hlrs1@discente.ifpe.edu.br>
2025-02-03 09:58:41 -0300

    Prática 5 - Parte 1: Configuração do Firebase

--- app/src/main/java/com/weatherapp/ui/MapPage.kt
+++ app/src/main/java/com/weatherapp/ui/MapPage.kt
@@ -1,21 +1,30 @@
 package com.weatherapp.ui
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapProperties
+import com.google.maps.android.compose.MapUiSettings
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
@@ -30,9 +39,17 @@ fun MapPage(modifier: Modifier = Modifier, viewModel: MainViewModel) {
     val caruaru = LatLng(-8.27, -35.98)
     val joaopessoa = LatLng(-7.12, -34.84)
 
+    val context = LocalContext.current
+    val hasLocationPermission by remember { mutableStateOf(
+        ContextCompat.checkSelfPermission(context,
+            Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+    }
+
     GoogleMap (modifier = Modifier.fillMaxSize(),
         onMapClick = { viewModel.add("Nova cidade", location = it) },
-        cameraPositionState = camPosState
+        cameraPositionState = camPosState,
+        properties = MapProperties(isMyLocationEnabled = hasLocationPermission),
+        uiSettings = MapUiSettings(myLocationButtonEnabled = true)
     ) {
 
         Marker(
